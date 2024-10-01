be2ca76167021cd50fac8c1ff7050c5e402eb6bb
Meesam <meesam.engineer@gmail.com>
2024-10-01 18:22:44 +0530

    Todo Ui is complete

--- BlazorUi/Program.cs
+++ BlazorUi/Program.cs
@@ -6,10 +6,7 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddRazorComponents();
-builder.Services
-    .AddBlazorise()
-    
- .AddFontAwesomeIcons();
+
 
 var app = builder.Build();
 
